1239af1b0e585de95da2c0ecdfa50d0c8ea9030e
adubasava <anzhalikad@gmail.com>
2024-03-01 14:44:13 +0300

    Change square backround color with mouse move

--- script.js
+++ script.js
@@ -2,7 +2,24 @@
 let container = document.getElementById('container');
 
 for (i = 0; i < number; i++){
-    div = document.createElement('div');
+    let div = document.createElement('div');
     div.classList.add('item');
     container.appendChild(div);
 }
+
+let divs = document.querySelectorAll('.item');
+
+divs.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+        let colorR = Math.floor(Math.random()*250);
+        let colorG = Math.floor(Math.random()*250);
+        let colorB = Math.floor(Math.random()*250);
+        item.style.backgroundColor = `rgb(${colorR},${colorG},${colorB})`;
+    });
+});
+
+// divs.forEach((item) => {
+//     item.addEventListener('mouseout', () => {
+//         item.style.backgroundColor = 'white';
+//     });
+// });

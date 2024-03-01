9b52f6d1b23f01b4d2bbb785e2766e990d8e8963
adubasava <anzhalikad@gmail.com>
2024-03-01 12:30:14 +0300

    Create a starting page with a 16x16 grid

---
+++ script.js
@@ -0,0 +1,8 @@
+﻿let number = 16*16;
+let container = document.getElementById('container');
+
+for (i = 0; i < number; i++){
+    div = document.createElement('div');
+    div.classList.add('item');
+    container.appendChild(div);
+}

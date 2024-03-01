bed004043ed75e85465b518314b08330a8fcc73e
adubasava <anzhalikad@gmail.com>
2024-03-01 16:49:20 +0300

    Add an option for a user to change resolution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-etch-a-sketch
+
+Project combines practice of Flex and JavaScript (DOM, events)
--- script.js
+++ script.js
@@ -1,36 +1,68 @@
-﻿let width = 960;
-let number = 64;
-let container = document.getElementById('container');
+﻿const WIDTH = 960;
+let number = 16;
+const container = document.querySelector('.container');
 
-for (i = 0; i < number*number; i++){
-    let div = document.createElement('div');
-    div.classList.add('item');
-    div.style.width = `${width / number}px`;
-    div.style.height = `${width / number}px`;
-    container.appendChild(div);
+createSketch(number);
+
+function createSketch(number) {
+    if (container.children.length > 0) {
+        let divs = document.querySelectorAll('.item');
+        removeDivs(divs);
+    }
+
+    for (i = 0; i < number*number; i++) {
+        let div = document.createElement('div');
+        div.classList.add('item');
+        div.style.width = `${WIDTH / number}px`;
+        div.style.height = `${WIDTH / number}px`;
+        container.appendChild(div);
+    }
+    
+    let divs = document.querySelectorAll('.item');
+    changeBackgroundColor(divs);
+    
+    let clear = document.querySelector('#clear');
+    clearBackground(clear, divs);
+
+    let res = document.querySelector('input#res');
+    let show = document.querySelector('#range');
+    res.addEventListener('input', () => {
+        show.textContent = `${res.value} x ${res.value}`;
+    })
+
+    let grid = document.querySelector('#grid');
+
+    grid.addEventListener('click', () => {
+        createSketch(document.querySelector('#res').value);
+    });
 }
 
-let divs = document.querySelectorAll('.item');
-
-divs.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-        let colorR = Math.floor(Math.random()*250);
-        let colorG = Math.floor(Math.random()*250);
-        let colorB = Math.floor(Math.random()*250);
-        item.style.backgroundColor = `rgb(${colorR},${colorG},${colorB})`;
-    });
-});
-
-// divs.forEach((item) => {
-//     item.addEventListener('mouseout', () => {
-//         item.style.backgroundColor = 'white';
-//     });
-// });
-
-clear = document.querySelector('#clear');
-
-clear.addEventListener('click', () => {
+function changeBackgroundColor(divs) {   
     divs.forEach((item) => {
-        item.style.backgroundColor = 'white';        
+        item.addEventListener('mouseover', () => {
+            let colorR = Math.floor(Math.random()*250);
+            let colorG = Math.floor(Math.random()*250);
+            let colorB = Math.floor(Math.random()*250);
+            item.style.backgroundColor = `rgb(${colorR},${colorG},${colorB})`;
+        });
     });
-})
+}
+
+function clearBackground(clear, divs) {
+    clear.addEventListener('click', () => {
+        divs.forEach((item) => {
+            item.style.backgroundColor = 'white';        
+        });
+    });
+}
+
+function removeDivs(divs) {
+    divs.forEach((item) => {
+        item.remove();        
+    });
+}
+
+
+
+
+
